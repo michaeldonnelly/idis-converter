0dce83aeb90bfc888b33a27e82fe4d36c1b3b165
Michael Donnelly <michaeld@hey.com>
2024-03-30 20:38:04 -0500

    Just convert ZIP to Excel

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿string directory = Directory.GetCurrentDirectory();
-IdisConverter.ConvertDirectory(directory, true);
+﻿if (args.Length < 1)
+{
+    Console.WriteLine("Pass in a ZIP file with an IDIS data extract.");
+}
+else
+{
+    string zipFile = args[0];
+    IdisExcelConverter.ConvertZip(zipFile, verbose: true);
+}
